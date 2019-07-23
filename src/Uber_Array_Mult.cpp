7602189b5be6947ea995769885633e30fa0a5e44
stephenjk41 <stephenjk41@gmail.com>
2019-07-22 21:24:31 -0400

    problem 2

---
+++ src/Uber_Array_Mult.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Given an array of integers, return a new array such that each element at index i 
+of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. 
+If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+*******************************************************************************/
+
+#include <iostream>
+#include <array>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    std::vector<int> a = {1,2,3,4,5};
+    
+    std::vector<int> sum;
+    for(int i = 0; i < a.size(); i++) {
+        int mult = 1;
+        for(int j = 0; j < a.size(); j++)
+        {
+            if(j != i) {
+                mult = mult * a[j];
+                // std::cout << mult << std::endl;
+            }  
+        }
+        sum.emplace_back(mult);
+    }
+    for(int i = 0; i < sizeof(a); i++) {
+        std::cout << sum.at(i) << std::endl;
+    }
+    return 0;
+}
