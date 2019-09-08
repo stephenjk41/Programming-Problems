ab7fb2051d85d9552c1cc3204f7d3a4c951aae70
stephenjk41 <stephenjk41@gmail.com>
2019-09-07 20:25:53 -0400

    added comments to two sigm problem

--- src/Two_Sigma_Random_5_7.cpp
+++ src/Two_Sigma_Random_5_7.cpp
@@ -16,22 +16,31 @@ int main()
 {
 
     std::cout << "Random number 1 - 5: " << rand5() << ". Random number 1 - 7: " << rand7();
-
     return 0;
 }
 
 int rand5()
 {
+    //random_device uniformly-distributed integer random number generator that produces non-deterministic
+    //random numbers.
     std::random_device dev5;
+    //A Mersenne Twister pseudo-random generator of 32-bit numbers with a state size of 19937 bits.
     std::mt19937 rng(dev5());
+    //This distribution produces random integers in a range [a,b] where each possible value has an equal likelihood of being produced.
+    //This is the distribution function that appears on many trivial random processes (like the result of rolling a die).
     std::uniform_int_distribution<std::mt19937::result_type> dist5(1, 5); // distribution in range [1, 6]
     return dist5(rng);
 }
 
 int rand7()
 {
+    //random_device uniformly-distributed integer random number generator that produces non-deterministic
+    //random numbers.
     std::random_device dev7;
+    //A Mersenne Twister pseudo-random generator of 32-bit numbers with a state size of 19937 bits.
     std::mt19937 rng(dev7());
+    //This distribution produces random integers in a range [a,b] where each possible value has an equal likelihood of being produced.
+    //This is the distribution function that appears on many trivial random processes (like the result of rolling a die).
     std::uniform_int_distribution<std::mt19937::result_type> dist7(1, 7); // distribution in range [1, 7]
     return dist7(rng);
 }
